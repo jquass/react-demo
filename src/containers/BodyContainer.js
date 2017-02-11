ea07ada5fa9123bdb257a6fca5d4b60f14aac17c
Jon Quass <jquass@bluestatedigital.com>
2017-02-11 15:57:12 -0500

    Adding basic architecture

---
+++ src/containers/BodyContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Body from '../components/Body';
+
+class BodyContainer extends Component {
+
+    render() {
+        return <Body />;
+    }
+}
+
+export default BodyContainer;
---
+++ src/containers/FooterContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Footer from '../components/Footer';
+
+class FooterContainer extends Component {
+
+    render() {
+        return <Footer />;
+    }
+}
+
+export default FooterContainer;
